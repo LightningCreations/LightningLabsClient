ea6eca88f8cb7ba6af24b668b4285a3038ee05c0
Ray Redondo <raydredondo@gmail.com>
2023-05-09 14:04:50 -0500

    electron enabled flag

--- src/preload.ts
+++ src/preload.ts
@@ -1,14 +1,16 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener("DOMContentLoaded", () => {
-  const replaceText = (selector: string, text: string) => {
-    const element = document.getElementById(selector);
-    if (element) {
-      element.innerText = text;
-    }
-  };
+  // const replaceText = (selector: string, text: string) => {
+  //   const element = document.getElementById(selector);
+  //   if (element) {
+  //     element.innerText = text;
+  //   }
+  // };
 
-  for (const type of ["chrome", "node", "electron"]) {
-    replaceText(`${type}-version`, process.versions[type as keyof NodeJS.ProcessVersions]);
-  }
+  // for (const type of ["chrome", "node", "electron"]) {
+  //   replaceText(`${type}-version`, process.versions[type as keyof NodeJS.ProcessVersions]);
+  // }
+  const electron_enabled_flag = document.getElementById("electron_enabled");
+  electron_enabled_flag.hidden = false;
 });
--- ui-ts/script.ts
+++ ui-ts/script.ts
@@ -1,1 +1,6 @@
-console.log("Test");
+window.onload = () => {
+    const electron_enabled_flag = document.getElementById("electron_enabled");
+    if (!electron_enabled_flag.hidden) {
+        console.log("Electron detected");
+    }
+}
