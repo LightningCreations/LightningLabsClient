8de25053a60c303d7c8a90b92cc3a4c7bd8ca928
Ray Redondo <raydredondo@gmail.com>
2023-05-26 07:25:16 -0500

    remove stale Rust files

--- target/debug/deps/client-3b2037b0d4d06825.d
+++
@@ -1,5 +0,0 @@
-C:\Users\matth\Documents\LightningLabsClient\target\debug\deps\client-3b2037b0d4d06825.rmeta: src\main.rs
-
-C:\Users\matth\Documents\LightningLabsClient\target\debug\deps\client-3b2037b0d4d06825.d: src\main.rs
-
-src\main.rs:
--- target/debug/deps/client-bd5b8cd1f8b97c45.d
+++
@@ -1,5 +0,0 @@
-C:\Users\matth\Documents\LightningLabsClient\target\debug\deps\client-bd5b8cd1f8b97c45.rmeta: src\main.rs
-
-C:\Users\matth\Documents\LightningLabsClient\target\debug\deps\client-bd5b8cd1f8b97c45.d: src\main.rs
-
-src\main.rs:
