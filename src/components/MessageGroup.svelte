b4aa8425bd9ef29d4f03b3b64156aad0c3a2304e
Isaac Kilbourne <isaac@kilbourne.ca>
2024-06-13 01:53:01 -0400

    Message log fills/scrolls bottom up, message sending
    Tweaked the chat log so that messages appear at the
    end of the chat log. Needed to use a dirty hack where
    I make a copy to reverse the list of messages, as
    `justify-end` decided it wanted to break the scrolling.
    
    Added text input for user texting. Pressing enter
    "sends" the message by adding it to the end of
    the message log. Svelte's reactivity takes care
    of re-grouping chat messages and everything, because...
    
    SVELTE IS FREAKING AWESOME!!!

--- src/components/MessageGroup.svelte
+++ src/components/MessageGroup.svelte
@@ -8,7 +8,7 @@
   $: messageContents = [firstMessage.content, ...otherMessages.map((msg) => msg.content)];
 </script>
 
-<div class="flex flex-row items-start w-full gap-4 px-4 py-2 flex-nowrap">
+<div class="flex flex-row items-start w-full gap-4 mb-4 flex-nowrap">
   <img
     src={firstMessage.authorImage}
     alt={'Profile picture for ' + firstMessage.author}
--- src/components/TextChat.svelte
+++ src/components/TextChat.svelte
@@ -1,10 +1,45 @@
 <script lang="ts">
-  import { groupedMessages } from '../lib/messages';
+  import { groupedMessages, messages } from '../lib/messages';
   import MessageGroup from './MessageGroup.svelte';
+
+  let userMessageContent: string = '';
+  const postMessage = () => {
+    // Don't send empty messages.
+    if (!userMessageContent) {
+      return;
+    }
+
+    messages.update((msgs) =>
+      msgs.concat({
+        createdAt: Date.now(),
+        author: 'Isaac Kilbourne',
+        authorImage: 'https://picsum.photos/seed/eyezick/48',
+        content: userMessageContent,
+      }),
+    );
+
+    userMessageContent = '';
+  };
 </script>
 
-<div class="flex flex-col">
-  {#each $groupedMessages as group}
-    <MessageGroup firstMessage={group[0]} otherMessages={group.slice(1)} />
-  {/each}
+<div class="flex flex-col justify-end h-full p-4 pt-0">
+  <div class="flex flex-col-reverse flex-1 overflow-y-auto">
+    <!-- 
+      We need to reverse the messages as we are using flex-col-reverse to
+      force the content to the end of the container. I would have liked to
+      use justify-end instead but for whatever reason that prevents scrollling.
+    -->
+    {#each $groupedMessages.reverse() as group}
+      <MessageGroup firstMessage={group[0]} otherMessages={group.slice(1)} />
+    {/each}
+  </div>
+
+  <form on:submit|preventDefault={postMessage} class="w-full">
+    <input
+      type="text"
+      name="message"
+      id="messageInput"
+      class="w-full p-4 py-2 rounded-lg text-zinc-800"
+      bind:value={userMessageContent} />
+  </form>
 </div>
