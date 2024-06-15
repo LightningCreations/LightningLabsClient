6efef5326d89ab12698be55e939732e4a548a5b7
Isaac Kilbourne <isaac@kilbourne.ca>
2024-06-15 00:29:48 -0400

    Dark mode resizer edges shoulld be dark lol

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,7 @@
     <ServerChooser />
   </Pane>
 
-  <PaneResizer class="flex justify-center py-1 border-t-2 border-b-2 border-zinc-300">
+  <PaneResizer class="flex justify-center py-1 border-t-2 border-b-2 border-zinc-300 dark:border-zinc-700">
     <span class="flex items-center w-12 h-3 text-center text-black rounded-lg bg-cyan-400">
       <div class="w-8 h-px mx-auto bg-black"></div>
     </span>
