bf20ce190358a10121b3ead9a9cccf13fa441ea2
Isaac Kilbourne <isaac@kilbourne.ca>
2024-06-15 01:18:13 -0400

    Resizer component, login form layout

---
+++ src/components/LabsPaneResizer.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { PaneResizer } from 'paneforge';
+
+  export let horizontal: boolean = false;
+</script>
+
+<PaneResizer
+  class="flex data-[horizontal=true]:flex-row flex-col justify-center p-1 outline-none data-[horizontal=false]:border-x-2 data-[horizontal=true]:border-y-2 border-zinc-300 dark:border-zinc-700 focus:outline focus:outline-cyan-500"
+  data-horizontal={horizontal}>
+  <span
+    class="flex items-center data-[horizontal=true]:h-3 data-[horizontal=true]:w-12 w-3 h-12 text-center text-black rounded-lg bg-cyan-500"
+    data-horizontal={horizontal}>
+    <div
+      class="data-[horizontal=true]:w-8 h-8 data-[horizontal=true]:h-px w-px mx-auto bg-black"
+      data-horizontal={horizontal}>
+    </div>
+  </span>
+</PaneResizer>
---
+++ src/components/LoginForm.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  let userEmail: string | undefined, userPassword: string | undefined;
+  $: userEmail = userPassword = undefined;
+
+  const login = () => {
+    console.table({ email: userEmail, password: userPassword });
+  };
+</script>
+
+<img alt="Lightning Labs Logo" src="https://placehold.co/216x72" class="mx-auto" width="216" height="72" />
+<form on:submit|preventDefault={login} class="flex flex-col items-center w-2/3 max-w-md gap-2 mx-auto">
+  <div class="flex flex-col w-full gap-1 items-left">
+    <label for="userEmail" class="text-zinc-700 dark:text-zinc-300">Email</label>
+    <input
+      type="email"
+      name="userEmail"
+      id="userEmail"
+      class="w-full p-6 py-3 rounded-lg outline-none focus:outline focus:outline-cyan-500 bg-zinc-300 dark:bg-zinc-800 text-zinc-800 dark:text-zinc-200 placeholder:text-zinc-700 dark:placeholder:text-zinc-400"
+      bind:value={userEmail} />
+  </div>
+
+  <div class="flex flex-col w-full gap-1 items-left">
+    <label for="userPassword" class="text-zinc-700 dark:text-zinc-300">Password</label>
+    <input
+      type="password"
+      name="userPassword"
+      id="userPassword"
+      class="w-full p-6 py-3 rounded-lg outline-none focus:outline focus:outline-cyan-500 bg-zinc-300 dark:bg-zinc-800 text-zinc-800 dark:text-zinc-200 placeholder:text-zinc-700 dark:placeholder:text-zinc-400"
+      bind:value={userPassword} />
+  </div>
+
+  <button
+    type="submit"
+    class="p-4 py-2 rounded-lg outline-none focus:outline focus:outline-cyan-500 bg-cyan-500 text-zinc-800 active:bg-cyan-600">
+    Log In
+  </button>
+</form>
