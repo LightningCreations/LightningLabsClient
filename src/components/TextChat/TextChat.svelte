f6bf821a4bf6b99eab32a38aa9dc5842e6d03d23
Isaac Kilbourne <isaac@kilbourne.ca>
2024-06-16 02:05:49 -0400

    Create user list component

--- src/components/TextChat/TextChat.svelte
+++ src/components/TextChat/TextChat.svelte
@@ -3,6 +3,7 @@
   import { onDestroy } from 'svelte';
   import { groupedMessages, messages } from '../../lib/messages';
   import { users } from '../../lib/users';
+  import UserList from './UserList.svelte';
 
   let placeholders: string[] = ['nice', 'cool', 'funny', 'fancy'];
   let placeholderIndex: number = 0;
@@ -35,30 +36,32 @@
   };
 </script>
 
-<div class="flex flex-col justify-end h-full p-4 pt-0">
-  <div class="flex flex-col-reverse flex-1 overflow-y-auto">
-    <!-- 
+<div class="flex flex-row h-full flex-nowrap">
+  <!-- Text chat -->
+  <div class="flex flex-col justify-end flex-grow h-full p-4 pt-0">
+    <div class="flex flex-col-reverse flex-1 overflow-y-auto">
+      <!-- 
       We need to reverse the messages as we are using flex-col-reverse to
       force the content to the end of the container. I would have liked to
       use justify-end instead but for whatever reason that prevents scrollling.
     -->
-    {#each $groupedMessages.reverse() as group}
-      <MessageGroup firstMessage={group[0]} otherMessages={group.slice(1)} />
-    {/each}
-  </div>
-
-  <form on:submit|preventDefault={postMessage} class="flex flex-row w-full gap-2 flex-nowrap">
-    <!-- This is temporary, for mocking purposes -->
-    <select
-      class="p-4 py-2 rounded-lg outline-none focus:outline focus:outline-cyan-500 bg-zinc-300 dark:bg-zinc-800 text-zinc-800 dark:text-zinc-200"
-      bind:value={selectedAuthorId}>
-      {#each $users as user}
-        <option value={user.id}>{user.displayName}</option>
+      {#each $groupedMessages.reverse() as group}
+        <MessageGroup firstMessage={group[0]} otherMessages={group.slice(1)} />
       {/each}
-    </select>
+    </div>
 
-    <!-- Todo: input:text has really really ugly handling for long text and multi-paragraph inputs -->
-    <!--
+    <form on:submit|preventDefault={postMessage} class="flex flex-row w-full gap-2 flex-nowrap">
+      <!-- This is temporary, for mocking purposes -->
+      <select
+        class="p-4 py-2 rounded-lg outline-none focus:outline focus:outline-cyan-500 bg-zinc-300 dark:bg-zinc-800 text-zinc-800 dark:text-zinc-200"
+        bind:value={selectedAuthorId}>
+        {#each $users as user}
+          <option value={user.id}>{user.displayName}</option>
+        {/each}
+      </select>
+
+      <!-- Todo: input:text has really really ugly handling for long text and multi-paragraph inputs -->
+      <!--
       This is not an easy problem to solve. Some good reading, particularly in the comments, can be found
       here: https://css-tricks.com/auto-growing-inputs-textareas/
 
@@ -69,18 +72,23 @@
 
       Another approach may be to simply pull in an existing library.
     -->
-    <input
-      type="text"
-      name="message"
-      id="messageInput"
-      class="w-full p-6 py-3 rounded-lg outline-none focus:outline focus:outline-cyan-500 bg-zinc-300 dark:bg-zinc-800 text-zinc-800 dark:text-zinc-200 placeholder:text-zinc-700 dark:placeholder:text-zinc-400"
-      placeholder={`something ${placeholders[placeholderIndex]}...`}
-      bind:value={message} />
+      <input
+        type="text"
+        name="message"
+        id="messageInput"
+        class="w-full p-6 py-3 rounded-lg outline-none focus:outline focus:outline-cyan-500 bg-zinc-300 dark:bg-zinc-800 text-zinc-800 dark:text-zinc-200 placeholder:text-zinc-700 dark:placeholder:text-zinc-400"
+        placeholder={`something ${placeholders[placeholderIndex]}...`}
+        bind:value={message} />
 
-    <!-- 
+      <!-- 
       This allows the user to press enter to submit the form. Turns out having multiple
       text inputs disables this behaviour, even though having a single text input is fine.
     -->
-    <input type="submit" hidden />
-  </form>
+      <input type="submit" hidden />
+    </form>
+  </div>
+
+  <div class="overflow-y-auto bg-zinc-800">
+    <UserList />
+  </div>
 </div>
---
+++ src/components/TextChat/UserList.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { users } from '../../lib/users';
+</script>
+
+<div class="flex flex-col w-64 gap-2 p-3 px-4 overflow-hidden">
+  {#each $users as user}
+    <div class="flex flex-row items-center gap-2">
+      <img
+        src={user.profileImageUrl}
+        alt={'Profile picture for ' + user.displayName}
+        class="inline-block text-sm rounded-full size-8" />
+
+      <!-- Ellipses overflow requires constrained width, whitespace nowrap, and overflow hidden -->
+      <p class="overflow-hidden font-semibold text-zinc-900 dark:text-zinc-200 text-ellipsis whitespace-nowrap">
+        {user.displayName}
+      </p>
+    </div>
+  {/each}
+</div>
