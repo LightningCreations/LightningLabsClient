f6d7f08e49b8d7f58babea2cb58fd688f57620ad
Ray Redondo <rdredond@lcdev.xyz>
2024-07-16 17:24:18 -0500

    start task store

--- src/App.svelte
+++ src/App.svelte
@@ -3,27 +3,38 @@
   import TextChat from './components/TextChat/TextChat.svelte';
   import LabsPaneResizer from './components/LabsPaneResizer.svelte';
   import LoginForm from './components/LoginForm.svelte';
+  import TaskList from './components/TaskList.svelte';
   import { PaneGroup, Pane } from 'paneforge';
 </script>
 
-<PaneGroup direction="vertical">
-  <Pane>
-    <PaneGroup direction="horizontal">
-      <Pane>
-        <ServerChooser />
-      </Pane>
-
-      <LabsPaneResizer />
-
-      <Pane class="flex flex-col items-center justify-center">
-        <LoginForm />
-      </Pane>
-    </PaneGroup>
+<PaneGroup direction="horizontal">
+  <Pane defaultSize={30}>
+    <TaskList />
   </Pane>
 
-  <LabsPaneResizer horizontal />
+  <LabsPaneResizer />
 
   <Pane>
-    <TextChat />
+    <PaneGroup direction="vertical">
+      <Pane>
+        <PaneGroup direction="horizontal">
+          <Pane>
+            <ServerChooser />
+          </Pane>
+
+          <LabsPaneResizer />
+
+          <Pane class="flex flex-col justify-center items-center">
+            <LoginForm />
+          </Pane>
+        </PaneGroup>
+      </Pane>
+
+      <LabsPaneResizer horizontal />
+
+      <Pane>
+        <TextChat />
+      </Pane>
+    </PaneGroup>
   </Pane>
 </PaneGroup>
--- src/components/TextChat/TextChat.svelte
+++ src/components/TextChat/TextChat.svelte
@@ -36,10 +36,10 @@
   };
 </script>
 
-<div class="flex flex-row h-full flex-nowrap">
+<div class="flex flex-row flex-nowrap h-full">
   <!-- Text chat -->
-  <div class="flex flex-col justify-end flex-grow h-full p-4 pt-0">
-    <div class="flex flex-col-reverse flex-1 overflow-y-auto">
+  <div class="flex flex-col flex-grow justify-end p-4 pt-0 h-full">
+    <div class="flex overflow-y-auto flex-col-reverse flex-1">
       <!-- 
       We need to reverse the messages as we are using flex-col-reverse to
       force the content to the end of the container. I would have liked to
@@ -50,7 +50,7 @@
       {/each}
     </div>
 
-    <form on:submit|preventDefault={postMessage} class="flex flex-row w-full gap-2 flex-nowrap">
+    <form on:submit|preventDefault={postMessage} class="flex flex-row flex-nowrap gap-2 w-full">
       <!-- This is temporary, for mocking purposes -->
       <select
         class="p-4 py-2 rounded-lg outline-none focus:outline focus:outline-cyan-500 bg-zinc-300 dark:bg-zinc-800 text-zinc-800 dark:text-zinc-200"
@@ -76,7 +76,7 @@
         type="text"
         name="message"
         id="messageInput"
-        class="w-full p-6 py-3 rounded-lg outline-none focus:outline focus:outline-cyan-500 bg-zinc-300 dark:bg-zinc-800 text-zinc-800 dark:text-zinc-200 placeholder:text-zinc-700 dark:placeholder:text-zinc-400"
+        class="p-6 py-3 w-full rounded-lg outline-none focus:outline focus:outline-cyan-500 bg-zinc-300 dark:bg-zinc-800 text-zinc-800 dark:text-zinc-200 placeholder:text-zinc-700 dark:placeholder:text-zinc-400"
         placeholder={`something ${placeholders[placeholderIndex]}...`}
         bind:value={message} />
 
@@ -88,7 +88,7 @@
     </form>
   </div>
 
-  <div class="flex-shrink-0 overflow-y-auto bg-zinc-300 dark:bg-zinc-800">
+  <div class="overflow-y-auto flex-shrink-0 bg-zinc-300 dark:bg-zinc-800">
     <UserList />
   </div>
 </div>
